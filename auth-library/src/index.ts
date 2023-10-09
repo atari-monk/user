debb60f7a7525dcbf3d353f8f681e931af6c0184
atari-monk <atari.monk1@gmail.com>
2023-10-09 22:21:04 +0200

    upd lib

--- auth-library/src/index.ts
+++ auth-library/src/index.ts
@@ -1,2 +1,6 @@
+//auth
 export { AuthContext } from './components/auth/AuthContext'
 export { AuthProvider } from './components/auth/AuthProvider'
+//login
+export { LoginEmailAndPassword } from './components/login/LoginEmailAndPassword'
+export { LoginGoogle } from './components/login/LoginGoogle'
