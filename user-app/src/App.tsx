dc7527ffc0c0bd33c755d6bd4a8be15e350e1c9b
atari-monk <atari.monk1@gmail.com>
2023-10-13 00:55:48 +0200

    login in app

--- user-app/src/App.tsx
+++ user-app/src/App.tsx
@@ -4,17 +4,33 @@ import About from './page/About'
 import Home from './page/Home'
 import './App.css'
 import ApiTester from './page/ApiTester'
+import { useContext, useState } from 'react'
+import { AuthContext, LoginGoogle, appConfig } from 'auth-library'
+import axios from 'axios'
 
-function App() {
+const App: React.FC = () => {
+  const { isLoggedIn } = useContext(AuthContext)
+  const [message, setMessage] = useState('')
+
   return (
     <Router>
       <div className="App-header">
+        <LoginGoogle
+          config={appConfig}
+          setMessage={setMessage}
+          axiosInstance={axios}
+        />
         <Navigation />
         <Routes>
           <Route path="/about" element={<About />} />
-          <Route path="/api-tester" element={<ApiTester />} />
           <Route path="/" element={<Home />} />
+          {isLoggedIn ? (
+            <Route path="/api-tester" element={<ApiTester />} />
+          ) : (
+            <Route path="/api-tester" element={<p>Log in to see pages</p>} />
+          )}
         </Routes>
+        {message && <p>{message}</p>}
       </div>
     </Router>
   )
