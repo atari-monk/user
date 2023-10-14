420ec97cec3edf91b9fa0fcd77311ab621cd752d
atari-monk <atari.monk1@gmail.com>
2023-10-15 00:44:13 +0200

    use ui lib

--- user-app/src/App.tsx
+++ user-app/src/App.tsx
@@ -5,34 +5,38 @@ import Home from './page/Home'
 import './App.css'
 import ApiTester from './page/ApiTester'
 import { useContext, useState } from 'react'
-import { AuthContext, LoginGoogle, appConfig } from 'auth-library'
+import { AuthContext, LoginGoogle, appConfig } from 'auth-lib'
 import axios from 'axios'
+import { DarkModeToggle, DefaultContainer } from 'ui-lib'
 
 const App: React.FC = () => {
   const { isLoggedIn } = useContext(AuthContext)
   const [message, setMessage] = useState('')
 
   return (
-    <Router>
-      <div className="App-header">
-        <LoginGoogle
-          config={appConfig}
-          setMessage={setMessage}
-          axiosInstance={axios}
-        />
-        <Navigation />
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<Home />} />
-          {isLoggedIn ? (
-            <Route path="/api-tester" element={<ApiTester />} />
-          ) : (
-            <Route path="/api-tester" element={<p>Log in to see pages</p>} />
-          )}
-        </Routes>
-        {message && <p>{message}</p>}
-      </div>
-    </Router>
+    <DefaultContainer className={`App`}>
+      <Router>
+        <div className="App-header">
+          <LoginGoogle
+            config={appConfig}
+            setMessage={setMessage}
+            axiosInstance={axios}
+          />
+          <DarkModeToggle />
+          <Navigation />
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Home />} />
+            {isLoggedIn ? (
+              <Route path="/api-tester" element={<ApiTester />} />
+            ) : (
+              <Route path="/api-tester" element={<p>Log in to see pages</p>} />
+            )}
+          </Routes>
+          {message && <p>{message}</p>}
+        </div>
+      </Router>
+    </DefaultContainer>
   )
 }
 
--- user-app/src/page/Home.tsx
+++ user-app/src/page/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { DefaultContainer } from 'ui-lib'
 
 const Home: React.FC = () => {
   return (
-    <div>
+    <DefaultContainer>
       <h2>Home Page</h2>
       <p>This is the home page content.</p>
-    </div>
+    </DefaultContainer>
   )
 }
 
