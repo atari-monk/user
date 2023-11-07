a7f79c5911a258f5284e77328702a833f70f494a
atari-monk <atari.monk1@gmail.com>
2023-11-07 14:32:57 +0100

    rename

--- user-app/src/component/AppEdit.tsx
+++ user-app/src/component/AppEdit.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import { IApp } from './IApp'
+import { IApp } from '../api/IApp'
 
 const initialFormData = {
   name: '',
--- user-app/src/component/AppList.tsx
+++ user-app/src/component/AppList.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
-import { IApp } from './IApp'
+import { IApp } from '../api/IApp'
 
 const AppList: React.FC = () => {
   const [apps, setApps] = useState<IApp[]>([])
